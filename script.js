e2560e74a01613762df5450459b627a61c4491e5
Christian Reall-Fluharty <creallfluharty@gmail.com>
2020-11-19 22:28:49 -0600

    hello react

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-class Hello extends React.Component{
-  render() {
-    return <p>Hello {this.props.name}</p>;
-  };
+class Greeter extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div id="Greeter">
+            <label for="name_input">What is your name? </label>
+            <input type="text" id="name_input"></input>
+            <button type="button" onClick={this.greet}>Submit</button>
+        </div>;
+    };
+
+    greet() {
+        alert("Hello, " + document.getElementById("name_input").value)
+    }
 }
 
 ReactDOM.render(
-  <Hello name='World'/>,
-  document.getElementById('root'),
+    <Greeter/>,
+    document.getElementById('root'),
 );
