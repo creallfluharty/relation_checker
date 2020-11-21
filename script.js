2e967089619bdb6dd519c50ace5ffda55dc7f0ca
Christian Reall-Fluharty <creallfluharty@gmail.com>
2020-11-21 15:11:12 -0600

    controlled components + "this" closures

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 class Greeter extends React.Component{
     constructor(props) {
         super(props);
+        this.state = {
+            name: '',
+        }
     }
     render() {
         return <div id="Greeter">
             <label htmlFor="name_input">What is your name? </label>
-            <input type="text" id="name_input"></input>
-            <button type="button" onClick={this.greet}>Submit</button>
+            <input type="text" id="name_input" onChange={event => this.changeInput(event)}></input>
+            <button type="button" onClick={event => this.greet(event)}>Submit</button>
         </div>;
     };
 
     greet() {
-        alert("Hello, " + document.getElementById("name_input").value)
+        alert("Hello, " + this.state.name);
+    }
+
+    changeInput(event) {
+        this.setState({
+            name: event.target.value,
+        });
     }
 }
 
