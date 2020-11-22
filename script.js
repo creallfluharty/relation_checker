e2e3a5b52764aa0b28153c5da38fba0c1d5bab7e
Christian Reall-Fluharty <creallfluharty@gmail.com>
2020-11-22 13:31:06 -0600

    add relationEntry componenet

--- script.js
+++ script.js
@@ -1,30 +1,68 @@
-class Greeter extends React.Component{
+function replaceNullWithEmptyList(maybeNull) {
+    return (maybeNull === null)? [] : maybeNull;
+}
+
+
+class RelationEntry extends React.Component{
     constructor(props) {
         super(props);
+
+        this.defaultText = '{}';
+        this.relationRegex = /^(\{(\(\w,\w\),)*(\(\w,\w\))\})|(\{\})$/;
+
         this.state = {
-            name: '',
+            relationStr: this.defaultText,
+            isValidInput: true,
+            relation: [],
         }
     }
+
     render() {
-        return <div id="Greeter">
-            <label htmlFor="name_input">What is your name? </label>
-            <input type="text" id="name_input" onChange={event => this.changeInput(event)}></input>
-            <button type="button" onClick={event => this.greet(event)}>Submit</button>
+        return <div id="relationEntry" className={`relationEntry ${this.state.isValidInput? '': 'in'}validRelationEntry`}>
+            <label htmlFor="relation_input">Enter your relation: </label>
+            <input type="text" id="relation_input"
+                value={this.state.relationStr} onChange={event => this.changeInput(event)}></input>
         </div>;
     };
 
-    greet() {
-        alert("Hello, " + this.state.name);
+    changeInput(event) {
+        try {
+            var relation = this.deserializeRelationStr(event.target.value);
+            var isValidInput = true;
+            console.log(`Got relation ${Array(...relation)}`);
+        } catch (error) {
+            console.log(error);
+            var relation = [];
+            var isValidInput = false;
+        }
+
+        this.setState({
+            relationStr: event.target.value,
+            relation: relation,
+            isValidInput: isValidInput,
+        });
     }
 
-    changeInput(event) {
-        this.setState({
-            name: event.target.value,
-        });
+    deserializeRelationStr(relationStr) {
+        relationStr = relationStr.replaceAll(' ', '');
+
+        if (!this.relationRegex.test(relationStr)) {
+            throw `Bad Relation: Relation ${relationStr} did not match pattern ${this.relationRegex}`;
+        }
+
+        var pairs = new Set(replaceNullWithEmptyList(relationStr.match(/(\(\w,\w\))/g)).map(pairStr =>
+            pairStr.match(/\w/g)
+        ));
+
+        return pairs;
+    }
+
+    getRelation() {
+        return this.state.relation;
     }
 }
 
 ReactDOM.render(
-    <Greeter/>,
+    <RelationEntry/>,
     document.getElementById('root'),
 );
